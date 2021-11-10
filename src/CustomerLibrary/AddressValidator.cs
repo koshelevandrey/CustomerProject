7260d0ac66ae2720fd9514249772ae2e0aca9543
koshelevandrey <tikr22@gmail.com>
2021-11-10 20:17:55 +0300

    fix: fixed validator classes

--- src/CustomerLibrary/AddressValidator.cs
+++ src/CustomerLibrary/AddressValidator.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CustomerLibrary
 {
-    class AddressValidator
+    public class AddressValidator
     {
+        public static List<string> Validate(Address address)
+        {
+            List<string> errors = new List<string>();
+            if (address.Line.Length > 100)
+            {
+                errors.Add("Line is longer than 100 char");
+            }
+            if ((address.Line2 != null) && (address.Line2.Length > 100))
+            {
+                errors.Add("Line 2 is longer than 100 char");
+            }
+            if (address.City.Length > 50)
+            {
+                errors.Add("City is longer than 50 char");
+            }
+            if (address.PostalCode.Length > 6)
+            {
+                errors.Add("Postal code is longer than 6 char");
+            }
+            if (address.State.Length > 20)
+            {
+                errors.Add("State is longer than 20 char");
+            }
+            if (!(address.Country == "United States" || address.Country == "Canada"))
+            {
+                errors.Add("Wrong country name (only United States and Canada are accepted)");
+            }
+
+            return errors;
+        }
     }
 }
--- src/CustomerLibrary/CustomerValidator.cs
+++ src/CustomerLibrary/CustomerValidator.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace CustomerLibrary
 {
-    class CustomerValidator
+    public class CustomerValidator
     {
+        public static List<string> Validate(Customer customer)
+        {
+            List<string> errors = new List<string>();
+            if ((customer.FirstName != null) && (customer.FirstName.Length > 50))
+            {
+                errors.Add("First name is longer than 50 char");
+            }
+            if (customer.LastName.Length > 50)
+            {
+                errors.Add("Last name is longer than 50 char");
+            }
+            if ((customer.Addresses == null) || (customer.Addresses.Count == 0))
+            {
+                errors.Add("Addresses list must contain at least 1 address");
+            }
+            if ((customer.PhoneNumber != null) && !(Regex.IsMatch(customer.PhoneNumber, @"^\+?[1-9]\d{1,14}$")))
+            {
+                errors.Add("Phone number is not in E.164 format");
+            }
+            if ((customer.Email != null) && !(Regex.IsMatch(customer.Email,
+                @"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")))
+            {
+                errors.Add("Email is not valid");
+            }
+            if ((customer.Notes == null) || (customer.Notes.Count == 0))
+            {
+                errors.Add("Notes cannot be empty, at least 1 note must be provided");
+            }
+
+            return errors;
+        }
     }
 }
